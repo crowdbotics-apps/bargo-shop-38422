96266df24ebb4d3b85567a4adccbd71c80324471
Crowdbotics <team@crowdbotics.com>
2023-01-24 14:17:30 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "bargo-shop-38422",
+  url: "https://bargo-shop-38422.botics.co",
+  api: "https://bargo-shop-38422.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
